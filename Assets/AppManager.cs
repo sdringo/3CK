faff6956da1674c37548d850f976677448d7ec8e
sdringo <sdringo9@gmail.com>
2016-12-07 16:10:46 +0900

    + 초기 스트립트 추가

---
+++ Assets/AppManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class AppManager : Entity
+{
+    private void OnApplicationQuit()
+    {
+        GameObject.Destroy(gameObject);
+    }
+
+    #region ILifeCycle Function
+    public override void initialize()
+    {
+        Application.targetFrameRate = 60;
+
+        GameObject.DontDestroyOnLoad(this.gameObject);
+
+        //GameObject sceneMgr = SceneManager.instanceObject;
+        //GameObject jobExecutor = JobExecutor.instanceObject;
+    }
+
+    public override void release()
+    {
+
+    }
+
+    public override void update()
+    {
+		if (Input.GetKey(KeyCode.Escape))
+			Application.Quit();
+    }
+    #endregion
+}
